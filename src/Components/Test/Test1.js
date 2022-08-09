0fe06a9100827175dcd94c46576ac321bff3ec06
VasundharaUpadhye <vasundharaupadhye25@gmail.com>
2022-08-09 19:43:54 +0530

    initial files

---
+++ src/Components/Test/Test1.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Test1 = () => {
+  return <div></div>;
+};
+
+export default Test1;
---
+++ src/Components/Test/Test2.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Test2 = () => {
+  return <div></div>;
+};
+
+export default Test2;
